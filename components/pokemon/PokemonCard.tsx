ef4cd229f8932a462b66a3b39419f2d669439356
DANIELSSF <daniel.santiago730@gmail.com>
2023-06-27 20:08:46 -0500

    Functional component PokemonCard created

---
+++ components/pokemon/PokemonCard.tsx
@@ -0,0 +1,36 @@
+import { FC } from 'react';
+
+import { Card, Grid, Row, Text } from '@nextui-org/react';
+
+import { SmallPokemon } from '../../interfaces/pokemon-list';
+import { useRouter } from 'next/router';
+
+interface Prop {
+  pokemon: SmallPokemon;
+}
+
+export const PokemonCard: FC<Prop> = ({ pokemon }) => {
+  const { img, name, id } = pokemon;
+
+  const router = useRouter();
+
+  const handleOnClick = (): void => {
+    router.push(`/pokemon/${id}`);
+  };
+
+  return (
+    <Grid xs={6} sm={3} md={2} xl={1} key={id}>
+      <Card isPressable isHoverable onClick={handleOnClick}>
+        <Card.Body css={{ p: 0 }}>
+          <Card.Image src={img} width="100%" height={140} alt={name} />
+        </Card.Body>
+        <Card.Footer>
+          <Row justify="space-between">
+            <Text transform="capitalize">{name}</Text>
+            <Text>#{id}</Text>
+          </Row>
+        </Card.Footer>
+      </Card>
+    </Grid>
+  );
+};
