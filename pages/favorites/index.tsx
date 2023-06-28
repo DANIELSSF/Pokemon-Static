f6ebd028cf71907d9ede393046a9b5996e88d7df
DANIELSSF <daniel.santiago730@gmail.com>
2023-06-27 20:12:05 -0500

    Page Favorites

---
+++ pages/favorites/index.tsx
@@ -0,0 +1,11 @@
+import { MainLayout } from '../../components/Layouts';
+
+const FavoritePage = () => {
+  return (
+    <MainLayout title="Pokemon | Favorites">
+      <h1>Hola Mundo</h1>
+    </MainLayout>
+  );
+};
+
+export default FavoritePage;
