28729f45c328762e5fab00edf4a3f5d7840b7f19
DANIELSSF <daniel.santiago730@gmail.com>
2023-06-28 23:18:10 -0500

    barrel file updated

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from 'react';
+
+import localFavorites from '../../utils/localFavorites';
+import { NoFavorite, FavoritePokemon } from '../../components/ui';
 import { MainLayout } from '../../components/Layouts';
 
 const FavoritePage = () => {
+  const [favoritePokemon, setFavoritePokemon] = useState<number[]>([]);
+
+  useEffect(() => {
+    setFavoritePokemon(localFavorites.pokemonsFavorites());
+  }, []);
+
   return (
     <MainLayout title="Pokemon | Favorites">
-      <h1>Hola Mundo</h1>
+      {favoritePokemon.length === 0 
+      ? ( <NoFavorite /> ) 
+      : ( <FavoritePokemon pokemons={favoritePokemon} /> ) 
+      }
     </MainLayout>
   );
 };
