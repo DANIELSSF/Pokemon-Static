6ddc425c127c76a3d353fa2419972502524b7710
DANIELSSF <daniel.santiago730@gmail.com>
2023-07-08 15:37:21 -0500

    Petition wrapped in a try catch

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -2,12 +2,16 @@ import { PokemonInfo } from '../interfaces';
 import { pokeApi } from '../api';
 
 export const getPokemonInfo = async (nameOrId: string) => {
-  const { data } = await pokeApi.get<PokemonInfo>(`/pokemon/${nameOrId}`);
+  try {
+    const { data } = await pokeApi.get<PokemonInfo>(`/pokemon/${nameOrId}`);
 
-  return {
-    sprites: data.sprites,
-    name: data.name,
-    id: data.id,
-    typePokemon: data.types[0].type.name,
-  };
+    return {
+      sprites: data.sprites,
+      name: data.name,
+      id: data.id,
+      typePokemon: data.types[0].type.name,
+    };
+  } catch (error) {
+    return null;
+  }
 };
