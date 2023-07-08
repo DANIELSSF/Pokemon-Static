03069922b9b40b45b8659152b7bd150d464bba11
DANIELSSF <daniel.santiago730@gmail.com>
2023-07-08 15:37:29 -0500

    Incremental static regeneration added

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -135,17 +135,29 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
     paths: pokemons151.map((id) => ({
       params: { id },
     })),
-    fallback: false,
+    // fallback: false,
+    fallback: 'blocking',
   };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { id } = params as { id: string };
+  const pokemon = await getPokemonInfo(id);
+
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
 
   return {
     props: {
-      pokemon: await getPokemonInfo(id),
+      pokemon,
     },
+    revalidate: 86400, // 60*60*24 - Cada 24hrs se regenera la pág
   };
 };
 
